0375fabe024049ca8a070452bb549085f8bd4c07
Brian Kang <brianhkang1@gmail.com>
2018-08-16 23:38:33 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,11 +1,14 @@
 def get_first_name_of_season_winner(data, season)
+  real_ans = ""
   data.each do |which_season, info|
     if which_season == season 
       if info["status"] == "Winner"
         ans = info["name"]
-        ans.split!
+        array = ans.split
+        real_ans = array[0]
       end
     end 
+  real_ans
 end
 
 def get_contestant_name(data, occupation)

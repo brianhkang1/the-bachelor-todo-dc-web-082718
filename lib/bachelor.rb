854a0c7a72a04124141a345cf6ac6294aec1038a
Brian Kang <brianhkang1@gmail.com>
2018-08-16 23:58:05 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -17,7 +17,21 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  real_ans = ""
+  data.each do |which_season, array_info|
+    if which_season == season 
+      array_info.each do |element|
+        element.each do |key, value|
+          if value == "Winner"
+            ans = element["name"]
+            array = ans.split
+            real_ans = array[0]
+          end
+        end
+      end
+    end
+  end
+  real_ans
 end
 
 def count_contestants_by_hometown(data, hometown)

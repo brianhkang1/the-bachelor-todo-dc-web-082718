8dbcb0e1fa10cda9f1dc160aa1a21936aeeff5f8
Brian Kang <brianhkang1@gmail.com>
2018-08-16 23:35:00 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 def get_first_name_of_season_winner(data, season)
   data.each do |which_season, info|
     if which_season == season 
-      if 
+      if info["status"] == "winner"
     end 
 end
 

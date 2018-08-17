cd1db653689e9631d5ba2a33279d5a4c15cdbf30
Brian Kang <brianhkang1@gmail.com>
2018-08-17 00:04:59 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -58,5 +58,17 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  real_ans = ""
+  data.each do |which_season, array_info|
+    if which_season == season
+      array_info.each do |element|
+        element.each do |key, value|
+          if value == occupation
+            real_ans = element["name"]
+          end
+        end
+      end
+    end
+  end
+  real_ans
 end
